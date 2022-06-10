d0a9aa24fee120e3d1dc9a2b32e5405d2868311b
James <jamespatterson1985@yahoo.com>
2022-06-10 02:26:45 -0400

    angular-portfolio: build: setup header server, transfer view from about to work, start setup for about and work html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { HeaderService } from './header.service';
 import { TooltipPosition } from '@angular/material/tooltip';
 import { Tab } from './tab.interface';
 import constants from '../state/constants.json';
@@ -14,19 +15,42 @@ export class HeaderComponent implements OnInit {
   imgUrl: string = constants['random-photo-url'];
   position = new FormControl(['left']);
   name: string = constants.name;
-  tabs: Tab[] = constants['header-tabs'];
-  tabSelected = new FormControl(
-    this.tabs.findIndex((t: Tab) => (t || {}).isActive)
-  );
+  tabs: Tab[] = [];
+  tabSelected = new FormControl();
 
-  constructor() {}
+  constructor(private HeaderService: HeaderService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getTabs();
+    this.getTab();
+  }
 
-  onClick = (tab: Tab, index: number) => {
-    this.tabSelected.setValue(new FormControl(index));
-    this.tabs.forEach((t: Tab, i: number) => {
-      (t || constants['header-default']).isActive = index === i ? true : false;
-    });
+  getTab = () => {
+    this.tabSelected = this.HeaderService.getTab();
+  };
+
+  setSelectedTab = (index: number) => {
+    this.tabSelected.setValue(index);
+    this.HeaderService.setTab(index);
+  };
+
+  getTabs = () => {
+    this.tabs = this.HeaderService.getTabs();
+  };
+
+  setTabs = (index: number) => {
+    this.HeaderService.setTabs(
+      this.tabs.filter((t: Tab, i: number) => {
+        (t || constants['header-default']).isActive =
+          index === i ? true : false;
+        return t;
+      })
+    );
+    this.tabs = this.HeaderService.getTabs();
+  };
+
+  onClick = (index: number) => {
+    this.setSelectedTab(index);
+    this.setTabs(index);
   };
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../header/header.service';
 
 @Component({
   selector: 'app-main',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  constructor() {}
+  constructor(private HeaderService: HeaderService) {}
 
   ngOnInit(): void {}
+
+  getTab = () => this.HeaderService.getTab().value;
 }
